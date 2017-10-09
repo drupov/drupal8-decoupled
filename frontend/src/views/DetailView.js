05ad3c52c6510c2228c247309bcb3143c662a21e
Dimitar Rupov <dimitar@precessionmedia.com>
2017-10-09 22:03:20 +0200

    Modify DetailView component

--- frontend/src/views/DetailView.js
+++ frontend/src/views/DetailView.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { gql, graphql } from 'react-apollo'
 
-export default class DetailView extends React.Component {
-  render() {
-    return <div>Detail</div>
+const query = gql`
+query ($id: String, $language: AvailableLanguages) {
+  nodeById(id: $id, language: $language) {
+    entityId
+    entityLabel,
+    entityCreated
   }
 }
+`
+
+class DetailView extends React.Component {
+  render() {
+    let { data } = this.props
+    console.log(this.props)
+    if (data.loading) { return <div>Loading...</div> }
+    return (
+      <div>
+        <h1>Message: {data.nodeById.entityId}</h1>
+        <p>{data.nodeById.entityCreated}</p>
+        <p>{data.nodeById.entityLabel}</p>
+      </div>
+    )
+  }
+}
+
+const queryOptions = {
+  options: props => ({
+    variables: {
+      id: props.match.params.id,
+      language: 'en'
+    },
+  }),
+}
+
+DetailView = graphql(query, queryOptions)(DetailView)
+export default DetailView
