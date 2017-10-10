a7074abf132135bec44130c5d02c4b42ad537c68
Dimitar Rupov <dimitar@precessionmedia.com>
2017-10-10 23:10:03 +0200

    Update CreateView (variables assignment missing)

--- frontend/src/views/CreateView.js
+++ frontend/src/views/CreateView.js
@@ -1,7 +1,68 @@
 import React from 'react'
+import { gql, graphql } from 'react-apollo'
 
-export default class CreateView extends React.Component {
-  render() {
-    return <div>Create</div>
+const mutation = gql`
+mutation ($input: NodeArticleCreateInput!) {
+  createNodeArticle(input: $input) {
+    violations {
+      path
+      message
+    }
+    errors
+    entity {
+      entityId
+      entityBundle
+      entityLabel
+    }
   }
 }
+`;
+
+/*
+{
+  "input": {
+    "title": "My cool title",
+    "comment": {
+      "status": "1"
+    }
+  }
+}
+*/
+
+class CreateView extends React.Component {
+  handleSubmit(e) {
+    e.preventDefault();
+    let formData = new FormData(this.form);
+    this.props
+      .mutate({variables: {input: formData.get('title')}})
+      .then(res => {
+        if (res.data.createNodeArticle.errors === null) {
+          window.location.replace(`/`);
+        }
+        else {
+          console.log(res.data.createNodeArticle.errors);
+        }
+      })
+      .catch(err => {
+        console.log('Network error!');
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Create</h1>
+        <form
+          ref={ref => (this.form = ref)}
+          onSubmit={e => this.handleSubmit(e)}
+        >
+          <textarea name="title" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+CreateView = graphql(mutation)(CreateView);
+export default CreateView
