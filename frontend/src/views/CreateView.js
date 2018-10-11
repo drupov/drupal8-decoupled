c1c2aaa7b51dce4901ac2af2d4a52409fefd7618
Dimitar Rupov <dimitar@precessionmedia.com>
2018-10-11 22:36:24 +0200

    Fix the CreateView component

--- frontend/src/views/CreateView.js
+++ frontend/src/views/CreateView.js
@@ -19,42 +19,32 @@ mutation ($input: ArticleInput!) {
 `;
 
 class CreateView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-      body: '',
-      field_more: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    return;
-    this.setState({ todo: e.target.value });
-  }
-
-  resetTodo() {
-    this.setState({
-      title: '',
-      body: '',
-      field_more: ''
-    });
-  }
-
   render() {
     return (
       <Mutation mutation={CREATE_NODE}>
         {createNode => (
           <form
+            ref={ref => (this.form = ref)}
             onSubmit={e => {
               e.preventDefault();
-              createNode({ input: {
-                title: this.state.title,
-                body: this.state.body,
-                field_more: this.state.more
-              }});
-              this.resetTodo();
+              let formData = new FormData(this.form);
+              createNode({ variables: {
+                input: {
+                  title: formData.get('title'),
+                  body: formData.get('body'),
+                  field_more: formData.get('field_more')
+              }}})
+              .then(res => {
+                if (res.data.createArticle.violations.length === 0) {
+                  window.location.replace(`/`);
+                }
+                else {
+                  console.log('Violations!');
+                }
+              })
+              .catch(err => {
+                console.log('Network error!');
+              });
             }}
           >
             <label htmlFor="title">Title</label>
@@ -63,74 +53,14 @@ class CreateView extends Component {
             <label htmlFor="body">Body</label>
             <textarea name="body" />
             <br />
-            <label htmlFor="more">More</label>
+            <label htmlFor="field_more">More</label>
             <input type="text" name="more" />
             <br />
-            }}>
             <button type="submit">Create article</button>
           </form>
         )}
       </Mutation>
     );
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    let formData = new FormData(this.form);
-    let input = {
-      title: formData.get('title'),
-      body: formData.get('body'),
-      field_more: formData.get('more')
-    };
-
-    /*this.props
-      .mutate({variables: {
-        input: {
-          title: formData.get('title'),
-          body: formData.get('body'),
-          field_more: formData.get('more')
-        }
-      }})
-      .then(res => {
-        console.log(res);
-        if (res.data.createArticle.errors.length === 0) {
-          window.location.replace(`/`);
-        }
-        else {
-          console.log(res.data.createArticle.errors);
-        }
-      })
-      .catch(err => {
-        console.log('Network error!');
-      });
-  }
-
-  render() {
-    return (
-      <Mutation mutation={CREATE_NODE}>
-        {(createNode, { data }) => (
-          <div>
-            <h1>Create</h1>
-            <form
-              ref={ref => (this.form = ref)}
-              onSubmit={e => this.handleSubmit(e)}
-            >
-              <label htmlFor="title">Title</label>
-              <input type="text" name="title" />
-              <br />
-              <label htmlFor="body">Body</label>
-              <textarea name="body" />
-              <br />
-              <label htmlFor="more">More</label>
-              <input type="text" name="more" />
-              <br />
-              }}>
-                <button type="submit">Submit</button>
-            </form>
-          </div>
-        )};
-      </Mutation>
-    )*/
   }
 }
 
